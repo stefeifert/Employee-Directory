8e44f6e8ebbe6b274bd3e3d7da785acb0722a0e9
Shiny <stefanie.eifert@yahoo.com>
2019-01-24 21:04:24 -0500

    updated files

--- app.js
+++ app.js
@@ -1,23 +1,120 @@
-$(".pagecontainer").addClass("hidden");
+let command = "";
 
+const print = function() {
+    let htmlStr = '';
+    for (let i = 0; i < employeeList.length; i++) {
+        htmlStr += '<div class="entry"><p> ${employeeList[i].name}</p> ${employeeList[i].officeNum}</p><p> $employeeList[i].phoneNum}</p></div>';
+        }
+    render(htmlStr);
+}
+ 
+const add = function() {
+    const userName = $('#name').val();
+    const officeNum = $('#office').val();
+    const phoneNum = $('#phone').val();
+    employeeList.push({
+        name: userName,
+        officeNum: officeNum,
+        phoneNum: phoneNum
+    })
+    print();    
+}
 
-const showNames = function(){
-    $(".pagecontainer").addClass("hidden");
-    $("#empList").removeClass("hidden");
-};
+const update = function() {
+    const userName = $('#name').val();
+    const officeNum = $('#office').val();
+    const phoneNum = $('#phone').val();
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === userName) {
+        employeeList[i].officeNum = officeNum;
+        employeelist[i].phoneNum = phoneNum;    
+        }
+    }        
+    print();    
+}
 
-$("#showNames").on("click", showAll);
+const verify = function() {
+    const userName = $('#name').val();
+    let htmlStr = 'no';
+    for (let i = 0; i < employeeList.length; i++) { 
+        if (employeeList[i].name === userName) {
+            htmlStr = 'yes';
+        }
+    }
+    render(htmlStr);
+}
 
-const showAddEmp = function(){
-    $(".pagecontainer").addClass("hidden");
-    $("#addEmp").removeClass("hidden");
-};
+const remove = function() {
+    const userName = $('#name').val();
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === username) {
+            employeeList.splice(i, 1);
+        }
+    }
+    print();
+}
 
-$("#addName").on("click", showAddEmp);
+const runCommand = function(event) {
+    // console.log('add');
+    event.preventDefault();
+    switch (command) {
+        case 'add':
+            add();
+            break;
+        case 'verify':
+            verify();
+            break;
+        case 'update':
+            PaymentRequestUpdateEvent();
+            break;
+        case 'delete':
+            remove();
+            break;
+    }
+}
 
-const verifyEmp = function(){
-    $(".pagecontainer").addClass("hidden");
-    $("#verifyEmp").removeClass("hidden");
-};
+const setView = function() {
+    $('#list').empty();
+    command = '';
+    $('form').hide();
+    print();
+}
 
-$("#verifyName").on("click", verifyEmp);
+const setAdd = function() {
+    $('#list').empty();
+    command = 'add';
+    $('form').show();
+    $('.extra-inputs').show();
+}
+
+const setVerify = function() {
+    $('#list').empty();
+    command = 'verify';
+    $('form').show();
+    $('.extra-inputs').hide();
+}
+
+const setUpdate = function() {
+    $('#list').empty()
+    command = 'update';
+    $('form').show();
+    $('.extra-inputs').show();
+}
+
+const setDelete = function() {
+    $('#list').empty();
+    command = 'delete';
+    $('form').show();
+    $('.extra-inputs').hide();
+}
+
+const render = function(htmlStr) {
+    $('#list').html(htmlStr);
+}
+
+$('#view').on('click', setView);
+$('#add').on('click', setAdd);
+$('#verify').on('click', setVerify);
+$('#update').on('click', setUpdate);
+$('#delete').on('click', setDelete);
+$('#submit').on('click', runCommand);
